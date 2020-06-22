976f76b1753d004da664f4a8a4917ec815bf39a2
unknown <Katherine2bk@gmail.com>
2020-06-22 12:45:09 -0700

    added basic structure of both actions, addFeature and removeFeature... need to figure out where to pass them

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export function addFeature(newFeature) {
+    return {
+        type: "ADD_FEATURE",
+        payload: newFeature
+    }
+}
+
+export function removeFeature(removeFeature) {
+    return {
+        type: "REMOVE_FEATURE",
+        payload: removeFeature
+    }
+}
